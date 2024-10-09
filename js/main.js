9ef4fbb822dd521194c7de09fb52d655e9407160
GlickYael <glick.yael3@gmail.com>
2024-10-09 13:46:47 +0300

    catagories

--- js/main.js
+++ js/main.js
@@ -1,19 +1,10 @@
 function main(){
     renderProducts(gNotebooks);
+    document.getElementById('craft').addEventListener('click', ()=>{renderProducts(gCraft)});
+    document.getElementById('notebooks').addEventListener('click', ()=>{renderProducts(gNotebooks)});
+    document.getElementById('jewelery').addEventListener('click', ()=>{renderFakeProducts('jewelery')});
+    document.getElementById('electronics').addEventListener('click', ()=>{renderFakeProducts('electronics')});
+    document.getElementById('womens-clothing').addEventListener('click', ()=>{renderFakeProducts("women's clothing")});
+    document.getElementById('mens-clothing').addEventListener('click', ()=>{renderFakeProducts("men's clothing")});
 }
 main();
-
-function rendergCraft(){
-    renderProducts(gCraft);
-}
-
-function rendergNotebooks(){
-    renderProducts(gNotebooks);
-}
-function renderJeweley(){
-    renderFakeProducts('jewelery');
-}
-
-function renderElectronics(){
-    renderFakeProducts('electronics');
-}
