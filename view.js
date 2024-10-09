b29947cea0ae8456642244843517899e297e42ac
GlickYael <glick.yael3@gmail.com>
2024-10-09 10:18:56 +0300

    Initial structure

---
+++ view.js
@@ -0,0 +1,16 @@
+const getProduct = (product)=>{
+    return `
+    <div class="product">
+        <h2>${product.title}</h2>
+        <p>${product.price}</p>
+    </div>
+    `;
+}
+
+const renderProducts = (products) => {
+    let productsStr = ``;
+    for(const product of products){
+        productsStr += getProduct(product);
+    }
+    document.getElementById('prod-area').innerHTML = productsStr;
+}
